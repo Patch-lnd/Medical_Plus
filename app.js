6b4b748d6649f20fd9dee7c353feb75c63aea1b9
Djeumeni Patchepia Arthur Samuel <djeumenipatchepiaarthursamuel@gmail.com>
2026-06-19 17:52:59 +0100

    Update app.js
    Setting u dependences, public folders and sesison

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require("express")
+const session = require("express-session")
+const path = require("path")
+
+const app = express();
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+// Statit Files 
+app.use(
+    express.static(
+        path.join(__dirname, "public")
+    )
+);
+
+// EJS configuration 
+app.set("view engine", "ejs");
+
+app.use(
+    sesion({
+        secret: "hospital_secret",
+        resave: false,
+        saveUninitialized: false,
+        cookie: {
+            maxAge: 2*60*60*1000
+            // 2hours
+        }
+    })
+)
+
+app.get("/", (req, res)=>{
+    res.render("login");
+})
+
+app.listen(3000, ()=>{
+    console.log("Serveur démaré sur le port 3000")
+});
